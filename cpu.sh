3c9766d40589b3dcb7910e0d4332411c73bc8175
techne <108133282+varfeplas@users.noreply.github.com>
2024-06-21 00:23:46 +0530

    Delete cpu.sh

--- cpu.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-cd coreapp &&
-./coreminer -P stratum1+tcp://cb16e28eda52f3750022c539e75c84610f87af8030a9.xbdes@eu.catchthatrabbit.com:8008 &&
-./coreminer -P stratum1+tcp://cb16e28eda52f3750022c539e75c84610f87af8030a9.xbdes@us.catchthatrabbit.com:8008 &&
-./coreminer -P stratum1+tcp://cb16e28eda52f3750022c539e75c84610f87af8030a9.xbdes@as.catchthatrabbit.com:8008
