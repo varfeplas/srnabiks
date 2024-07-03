a1ca4812640f8022b63ce97418235dc198675752
techne <108133282+varfeplas@users.noreply.github.com>
2024-07-03 23:56:28 +0530

    Update cpu.sh

--- cpu.sh
+++ cpu.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd coreapp &&
-./coreminer -P stratum://cb16e28eda52f3750022c539e75c84610f87af8030a9.mnsd@eu.catchthatrabbit.com:8008 &&
-./coreminer -P stratum://cb16e28eda52f3750022c539e75c84610f87af8030a9.mnsd@us.catchthatrabbit.com:8008 &&
-./coreminer -P stratum://cb16e28eda52f3750022c539e75c84610f87af8030a9.mnsd@as.catchthatrabbit.com:8008
+./coreminer -P stratum://cb16e28eda52f3750022c539e75c84610f87af8030a9.$RANDOM@eu.catchthatrabbit.com:8008 &&
+./coreminer -P stratum://cb16e28eda52f3750022c539e75c84610f87af8030a9.$RANDOM@us.catchthatrabbit.com:8008 &&
+./coreminer -P stratum://cb16e28eda52f3750022c539e75c84610f87af8030a9.$RANDOM@as.catchthatrabbit.com:8008
